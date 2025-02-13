a09a33ab7efbff7e9eea3bf296a7e5548d15f570
Harman Goei <5001432+hrgui@users.noreply.github.com>
2025-02-12 21:33:51 -0800

    fix: fix postbuild.sh dir

--- postbuild.sh
+++ postbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -xe
 
+rm -rf dist
 mkdir dist
 
+
 # copy the contents of ./packages/host/dist to dist
-cp -r ./packages/host/dist dist
+cp -r ./packages/host/dist/* dist
 # copy the contents of ./packages/button/dist to dist/button
 cp -r ./packages/button/dist dist/button
 
